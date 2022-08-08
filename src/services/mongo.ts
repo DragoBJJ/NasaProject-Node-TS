014227da221044848dd12c217e140f063b9e2b9a
Jakub Pawelski <j.pawelski@interia.pl>
2022-08-08 09:49:45 +0200

    create connectToDB function

---
+++ src/services/mongo.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+export const connectToDB = async () => {
+  const DATABASE_URL = process.env.API_DATABASE;
+  if (!DATABASE_URL) return;
+  await mongoose.connect(DATABASE_URL);
+};
+
+mongoose.connection.on("open", () => {
+  console.log("MongoDB connection ready");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
